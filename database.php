2c1d3cbad6e11f071000d2022bf458971c6a5f50
Petri Salo <petri.salo@hyria.fi>
2020-11-12 10:35:32 +0200

    Tehty tarvittavat muutokset PHP-koodiin

--- database.php
+++ database.php
@@ -17,7 +17,7 @@ $password = "";
 *   Tässä yhdistetään tietokantaan
 */
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=voting", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=test", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
@@ -27,28 +27,28 @@ try {
 /*
 *   Jos pyynnössä on mukava arvo nimellä 'vote', tallennetaan äänestys tietokantaan
 */
-if (isset($_POST['vote'])) {
+if (isset($_POST['tuote'])) {
     // Haetaan äänestyksen valinta
-    $vote = $_POST['vote'];
+    $tuote = $_POST['tuote'];
     // Haetaan käyttäjän ip-osoite
     $ip = $_SERVER['REMOTE_ADDR'];
 
     // Määritellään SQL-kysely
-    $sql = "INSERT INTO votes (vote, ip) VALUES (?, ?)";
+    $sql = "INSERT INTO kauppalista (tuote, ip) VALUES (?, ?)";
     // Lähetetään kysely palvelimelle
     $sttm = $conn->prepare($sql);
     // Suoritetaan kysely muuttujiin asetetuilla arvoilla
-    $sttm->execute(array($vote, $ip));
+    $sttm->execute(array($tuote, $ip));
 }
 
 // SQL-kysely, jolla haetaan eri vaihtoehtojen äänimäärät
-$sql = "SELECT vote, count(*) as 'count' FROM votes GROUP BY vote";
+$sql = "SELECT tuote FROM kauppalista";
 
 // Luodaan tyhjä sanakirja
 $data = array();
 // Haetaan tietokannasta äänimäärät
 foreach($conn->query($sql) as $row) {
-    $data[$row['vote']] = $row['count'];
+    array_push($data, $row['tuote']);
 }
 
 // Palautetaan JSON enkoodattu data
